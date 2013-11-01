0c4881b37e992cdb6c17f29c3ce0d83d3f7fcb6a
Sergey Krauchenia <skrauchenia@exadel.com>
2013-11-01 18:41:19 +0300

    initial plugin structure

---
+++ src/main/java/com/kupal/vuze/mtp/MoveToPlayerPlugin.java
@@ -0,0 +1,16 @@
+package com.kupal.vuze.mtp;
+
+import org.gudy.azureus2.plugins.Plugin;
+import org.gudy.azureus2.plugins.PluginException;
+import org.gudy.azureus2.plugins.PluginInterface;
+
+/**
+ * @author skrauchenia
+ */
+public class MoveToPlayerPlugin implements Plugin {
+
+    @Override
+    public void initialize(PluginInterface pluginInterface) throws PluginException {
+        System.out.println("Yo!!!");
+    }
+}
